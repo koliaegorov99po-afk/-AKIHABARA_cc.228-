0473f54ac21563e09a3c36e275f15a9c58f1114b
koliaegorov99po-afk <koliaegorov99.po@gmail.com>
2026-09-10 01:44:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-Const express = require("express");
+const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const sqlite3 = require("sqlite3").verbose();
